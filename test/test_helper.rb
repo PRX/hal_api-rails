e1497c1ef228bf7811041f67869f4646c2509d73
Andrew Kuklewicz <andrew@beginsinwonder.com>
2016-01-14 16:02:30 -0500

    Fix missing raitie methods by requiring railties mixins before hal_api

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,6 +1,9 @@
 $LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
-require 'hal_api/rails'
+require "action_controller/railtie"
+require "action_view/railtie"
 require 'active_support/all'
+
+require 'hal_api/rails'
 
 require 'minitest/spec'
 require 'minitest/autorun'
