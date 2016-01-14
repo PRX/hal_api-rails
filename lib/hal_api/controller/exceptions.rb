90d45353415c3590005df90ad479142e2d581f18
Andrew Kuklewicz <andrew@beginsinwonder.com>
2016-01-14 15:59:32 -0500

    Add new exception handling methods

---
+++ lib/hal_api/controller/exceptions.rb
@@ -0,0 +1,43 @@
+require 'action_dispatch/http/request'
+require 'action_dispatch/middleware/exception_wrapper'
+
+# Since we are taking over exception handling, make sure we log exceptions
+# https://github.com/rails/rails/blob/4-2-stable/actionpack/lib/action_dispatch/middleware/debug_exceptions.rb
+module HalApi::Controller::Exceptions
+  def respond_with_error(exception)
+    wrapper = ::ActionDispatch::ExceptionWrapper.new(env, exception)
+    log_error(env, wrapper)
+
+    error = exception.is_a?(HalApi::Errors::ApiError) ? exception : HalApi::Errors::ApiError.new
+    respond_with(
+      error,
+      status: error.status,
+      represent_with: HalApi::Errors::Representer
+    )
+  end
+
+  def log_error(env, wrapper)
+    logger = env['action_dispatch.logger'] || ActiveSupport::Logger.new($stderr)
+    return unless logger
+
+    exception = wrapper.exception
+
+    trace = wrapper.application_trace
+    trace = wrapper.framework_trace if trace.empty?
+
+    ActiveSupport::Deprecation.silence do
+      message = "\n#{exception.class} (#{exception.message}):\n"
+      message << exception.annoted_source_code.to_s if exception.respond_to?(:annoted_source_code)
+      message << "  " << trace.join("\n  ")
+      logger.fatal("#{message}\n\n")
+    end
+  end
+
+  module ClassMethods
+    def hal_rescue_standard_errors
+      rescue_from StandardError do |error|
+        respond_with_error(error)
+      end
+    end
+  end
+end
