f3624cf34304ec1dcb11f10a6ae78b7be9b3cc97
Andrew Kuklewicz <andrew@beginsinwonder.com>
2016-05-10 18:16:36 -0400

    Add proper load order for testing

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -3,6 +3,8 @@ require "action_controller/railtie"
 require "action_view/railtie"
 require 'active_support/all'
 
+require 'responders'
+require 'roar-rails'
 require 'hal_api/rails'
 
 require 'minitest/spec'
