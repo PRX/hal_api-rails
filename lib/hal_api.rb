2ec8350515c3a8b14ecb2a55dc59b30695b747f1
Andrew Kuklewicz <andrew@beginsinwonder.com>
2016-05-10 17:50:47 -0400

    This require causes loading issues

--- lib/hal_api.rb
+++ lib/hal_api.rb
@@ -2,6 +2,5 @@ module HalApi
   require 'hal_api/paged_collection'
   require 'hal_api/errors'
   require 'hal_api/controller'
-  # require 'hal_api/representer'
   require 'hal_api/represented_model'
 end
