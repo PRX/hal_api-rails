fa9a1871c0cacb19efcd112772342514aa9627e5
cavis <ryancavis@gmail.com>
2016-07-20 09:35:17 -0600

    Fix a POST request render error, and add traces to representer

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,5 +1,6 @@
 $LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
 require "action_controller/railtie"
+# require "action_controller/test_case"
 require "action_view/railtie"
 require 'active_support/all'
 
@@ -7,5 +8,21 @@ require 'responders'
 require 'roar-rails'
 require 'hal_api/rails'
 
+require 'pry-byebug'
 require 'minitest/spec'
 require 'minitest/autorun'
+
+ENV['RAILS_ENV'] = 'test'
+
+module Dummy
+  class Application < Rails::Application
+    config.encoding = 'utf-8'
+    config.eager_load = false
+    config.active_support.test_order = :random
+    config.secret_token = '1234'
+    config.secret_key_base = '5678'
+  end
+end
+Dummy::Application.initialize!
+
+require 'rails/test_help'
