d8fceaafee442e8fe731bb33653bbc84c3aa2a14
Sam Vevang <sam.vevang@gmail.com>
2019-08-12 15:39:10 -0500

    set up a new default api responder

---
+++ lib/hal_api/responders/api_responder.rb
@@ -0,0 +1,19 @@
+require 'roar/rails/responder'
+
+module HalApi::Responders
+  class ApiResponder < Roar::Rails::Responder
+    def api_behavior
+      raise MissingRenderer.new(format) unless has_renderer?
+
+      if post?
+        display(resource, status: :created)
+      elsif put?
+        display(resource, status: :ok)
+      elsif delete?
+        display(resource, status: :no_content)
+      else
+        super
+      end
+    end
+  end
+end
